8bbf35e2c8a53202dd58eb32bf48acaa6c0d7802
Matt Mauro <matt.mauro@gmail.com>
2016-11-17 10:38:50 -0500

    Initial Commit

---
+++ CoverFlow/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoverFlow
+//
+//  Created by Matthew Mauro on 2016-11-17.
+//  Copyright © 2016 Matthew Mauro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CoverFlow/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoverFlow
+//
+//  Created by Matthew Mauro on 2016-11-17.
+//  Copyright © 2016 Matthew Mauro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
